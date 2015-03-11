b7191fe88a70f1f8be63f6cf0bb68c738f505fbc
Christoph Haag <ChrisH.93@web.de>
2015-03-11 23:45:12 +0100

    o uses webcolors instead of the old utils.py for python2.7 compatibility and ease

--- mediatable_web/models.py
+++ mediatable_web/models.py
@@ -1,15 +1,16 @@
 from django.db import models
-from mediatable_web.util import *
+import webcolors
+
 
 class SimpleColor(models.Model):
-    hex_color = models.CharField(max_length = 7)
-    date_of_creation = models.DateTimeField(auto_now_add = True)
+    hex_color = models.CharField(max_length=7)
+    date_of_creation = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return self.hex_color
 
     def set_color_from_rgb(self, rgb):
-        self.hex_color = rgb_to_hex(rgb)
+        self.hex_color = webcolors.rgb_to_hex(rgb)
 
     def set_default_color(self):
-        self.hex_color = rgb_to_hex((0,0,0))
+        self.hex_color = webcolors.rgb_to_hex((0, 0, 0))
