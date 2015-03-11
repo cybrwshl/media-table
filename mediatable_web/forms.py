a7cea675578299fa8ca18ad74d031d32661d5713
Christoph Haag <ChrisH.93@web.de>
2015-03-11 23:44:04 +0100

    o formatting fixed

--- mediatable_web/forms.py
+++ mediatable_web/forms.py
@@ -1,5 +1,6 @@
 from django import forms
 
+
 class SimpleColorForm(forms.Form):
     red = forms.IntegerField(min_value=0, max_value=255)
     green = forms.IntegerField(min_value=0, max_value=255)
