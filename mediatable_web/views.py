118c0a735d3c6ddb123650b396019d82d5aa409e
Christoph Haag <ChrisH.93@web.de>
2015-03-11 23:47:09 +0100

    o uses webcolors and sends message with ZMQ after a color change

--- mediatable_web/views.py
+++ mediatable_web/views.py
@@ -1,16 +1,18 @@
-from django.shortcuts import render
+import logging
+
 from django.http import HttpResponse
 from django.template import RequestContext, loader
-from mediatable_web.models import SimpleColor
-from mediatable_web.util import rgb_to_hex, hex_to_rgb
-from mediatable_web.forms import SimpleColorForm
+import webcolors
 
-import logging
+from mediatable_web.models import SimpleColor
+from mediatable_web.forms import SimpleColorForm
+from zmqcom import send_color_json
+
 
 logger = logging.getLogger(__name__)
 
-def index(request):
 
+def index(request):
     if request.method == 'POST':
         form = SimpleColorForm(request.POST)
         if form.is_valid():
@@ -18,7 +20,7 @@ def index(request):
             r = form.cleaned_data['red']
             g = form.cleaned_data['green']
             b = form.cleaned_data['blue']
-            new_color.set_color_from_rgb((r,g,b))
+            new_color.set_color_from_rgb((r, g, b))
 
             # first delete all existing entries
             # because we only want one entry at a time, which is the current color
@@ -28,21 +30,23 @@ def index(request):
             new_color.save()
         else:
             logger.debug(form.errors)
-    
+
     current = get_current_or_create_default_color()
-    red, green, blue = hex_to_rgb(current.hex_color)
+    red, green, blue = webcolors.hex_to_rgb(current.hex_color)
+
+    # send a message via zmq
+    send_color_json((red, green, blue))
+
     form = SimpleColorForm()
     context = RequestContext(request, {'current': current, 'red': red, 'green': green, 'blue': blue, 'form': form})
-
-    logger.debug(context)
 
     template = loader.get_template('mediatable_web/index.html')
     return HttpResponse(template.render(context))
 
+
 def get_current_or_create_default_color():
     results = SimpleColor.objects.order_by('-date_of_creation')[:1]
 
-    current = None
     if results:
         current = results[0]
     else:
