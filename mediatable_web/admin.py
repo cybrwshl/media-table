f0c9f59d2d2647219c9925a7c87071e66f4a89e9
Christoph Haag <ChrisH.93@web.de>
2015-03-11 23:43:26 +0100

    o formatting fixed

--- mediatable_web/admin.py
+++ mediatable_web/admin.py
@@ -1,8 +1,11 @@
 from django.contrib import admin
+
 from mediatable_web.models import SimpleColor
+
 
 class SimpleColorAdmin(admin.ModelAdmin):
     readonly_fields = ['date_of_creation']
     list_display = ['hex_color', 'date_of_creation']
+
 
 admin.site.register(SimpleColor)
