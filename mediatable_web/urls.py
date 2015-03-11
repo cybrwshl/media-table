c88d335bdab3fc7c4e3bcc2d22b129c08f7eed91
Christoph Haag <ChrisH.93@web.de>
2015-03-11 23:45:49 +0100

    o formatting fixed

--- mediatable_web/urls.py
+++ mediatable_web/urls.py
@@ -1,6 +1,6 @@
 from django.conf.urls import patterns, url
+
 from mediatable_web import views
 
-urlpatterns = patterns('',
-    url(r'^$', views.index, name='index'),
-)
+
+urlpatterns = patterns('', url(r'^$', views.index, name='index'), )
